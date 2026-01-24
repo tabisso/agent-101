25a6ccac2206a4ad78181fb006b22ff59fd47b76
tabisso <tabissobsa@gmail.com>
2026-01-23 22:49:15 -0500

    complited the bussies schemas and chain

---
+++ schemas/output_schema.py
@@ -0,0 +1,11 @@
+
+from pydantic import BaseModel, Field
+
+
+
+class BussinessOverview(BaseModel):
+    summary: str = Field(description="clear one-paragraph business summary")
+    primary_target_audience: str = Field(description="specific audience description(one primary target)")
+    core_pain_point: str = Field(description="Main problem this audience has(One core piain point)")
+    unque_value_proposition: str = Field(description="Why this bussiness wins(One clear advantage)")
+    not_priority: str = Field(description="What should be avoided or deprioritized ")()
